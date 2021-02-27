d7c7e547efbd9e774f639bd1176c1acce6e9ab58
Saleem Abdulrasool <compnerd@compnerd.org>
2021-02-27 13:54:12 -0800

    CPE: make more portable, add missing copyright
    Add a missing copyright header to CPE.h.  C does not permit an empty
    translation unit.  Add a single declaration to the translation unit to
    ensure that the translation unit is portable.

--- Sources/CPE/include/CPE.h
+++ Sources/CPE/include/CPE.h
@@ -1,3 +1,9 @@
+/**
+ * Copyright 2020 Saleem Abdulrasool <compnerd@compnerd.org>
+ * All Rights Reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ **/
 
 #ifndef CPE_CPE_h
 #define CPE_CPE_h

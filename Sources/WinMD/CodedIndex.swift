803d1fa9a8aeaa31f2bd8dbf77d1f02e26a15785
Saleem Abdulrasool <compnerd@compnerd.org>
2021-06-25 16:29:48 -0700

    WinMD: add some documentation for `CodedIndex` (NFC)

--- Sources/WinMD/CodedIndex.swift
+++ Sources/WinMD/CodedIndex.swift
@@ -2,9 +2,17 @@
 // All rights reserved.
 // SPDX-License-Identifier: BSD-3-Clause
 
-
+/// A compressed index which is an index into a predefined set of tables.
+///
+/// The tagged-union is formed by encoding the descriminator in the bottom
+/// log(n) bits and the index in the remaining bits.  The raw value is either
+/// 16-bits if all the tables use a 16-bit index or 32-bit otherwise.
 internal protocol CodedIndex: Hashable {
-  /// The tables that the coded index may index.
+  /// The tables that the `CodedIndex` descriminates across.
+  ///
+  /// The order of the tables is important.  The tag identifies the table and
+  /// indexes through them, therefore, it is critical the index of the table
+  /// corresponds to the tag value.
   static var tables: [TableBase.Type] { get }
 
   /// The value of the coded index.
@@ -15,14 +23,17 @@ internal protocol CodedIndex: Hashable {
 }
 
 extension CodedIndex {
+  /// The mask to extract the descriminator from the `CodedIndex`.
   internal static var mask: Int {
     (1 << (64 - (Self.tables.count - 1).leadingZeroBitCount)) - 1
   }
 
+  /// The table descriminator used to select between the tables.
   internal var tag: Int {
     self.rawValue & Self.mask
   }
 
+  /// The row for the selected table that the index identifies.
   internal var row: Int {
     self.rawValue >> Self.mask.nonzeroBitCount
   }

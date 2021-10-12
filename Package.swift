7a3faf7dddf24ef96fee22892700dfd3b799ae43
Saleem Abdulrasool <compnerd@compnerd.org>
2021-10-12 03:00:10 +0000

    WinMD: adopt `OrderedDictionary`
    This uses `OrderedDictionary` rather than `Dictionary` with a custom
    sort method.  This allows a slightly more efficient implementation of
    the column traversal.

--- Package.swift
+++ Package.swift
@@ -8,14 +8,20 @@ let SwiftWinMD = Package(
     .executable(name: "winmd-inspect", targets: ["winmd-inspect"]),
   ],
   dependencies: [
-    .package(url: "http://github.com/apple/swift-argument-parser", from: "0.3.2"),
+    .package(url: "http://github.com/apple/swift-argument-parser",
+             from: "0.3.2"),
+    .package(url: "https://github.com/apple/swift-collections.git",
+             .upToNextMinor(from: "1.0.0")),
   ],
   targets: [
     .target(name: "CPE", dependencies: []),
 
     // WinMD
     .target(name: "WinMD",
-            dependencies: ["CPE"],
+            dependencies: [
+              "CPE",
+              .product(name: "OrderedCollections", package: "swift-collections"),
+            ],
             swiftSettings: [
               .unsafeFlags([
                 "-Xfrontend", "-validate-tbd-against-ir=none",

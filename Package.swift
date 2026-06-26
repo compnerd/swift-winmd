d745a30c11b16369acfdb1f31d05669cc69d3f57
Saleem Abdulrasool <compnerd@compnerd.org>
2026-06-26 12:29:05 -0700

    build: adjust style drift in Package.swift (NFC)

--- Package.swift
+++ Package.swift
@@ -2,63 +2,63 @@
 
 import PackageDescription
 
-let SwiftWinMD = Package(
-  name: "SwiftWinMD",
-  platforms: [
-    .macOS(.v26),
-  ],
-  products: [
-    .executable(name: "winmd-inspect", targets: ["winmd-inspect"]),
-    .library(name: "SQL", targets: ["SQL"]),
-  ],
-  dependencies: [
-    .package(url: "https://github.com/apple/swift-argument-parser",
-             from: "1.5.0"),
-  ],
-  targets: [
-    .target(name: "CPE", dependencies: []),
-
-    // SQL
-    .target(name: "SQL", dependencies: [],
-            swiftSettings: [
-              .enableExperimentalFeature("Lifetimes"),
-            ]),
-    .testTarget(name: "SQLTests", dependencies: ["SQL"],
-            swiftSettings: [
-              .enableExperimentalFeature("Lifetimes"),
-            ]),
-
-    // WinMD
-    .target(name: "WinMD",
-            dependencies: [
-              "CPE",
+let _ =
+    Package(name: "SwiftWinMD",
+            platforms: [
+              .macOS(.v26),
             ],
-            swiftSettings: [
-              .enableExperimentalFeature("Lifetimes"),
-            ]),
-    .testTarget(name: "WinMDTests", dependencies: ["WinMD"],
-            swiftSettings: [
-              .enableExperimentalFeature("Lifetimes"),
-            ]),
+            products: [
+              .executable(name: "winmd-inspect", targets: ["winmd-inspect"]),
+              .library(name: "SQL", targets: ["SQL"]),
+            ],
+            dependencies: [
+              .package(url: "https://github.com/apple/swift-argument-parser",
+                       from: "1.5.0"),
+            ],
+            targets: [
+              .target(name: "CPE", dependencies: []),
 
-    // winmd-inspect
-    .executableTarget(name: "winmd-inspect",
-            dependencies: [
-              "SQL",
-              "WinMD",
-              .product(name: "ArgumentParser", package: "swift-argument-parser"),
-            ],
-            swiftSettings: [
-              .enableExperimentalFeature("Lifetimes"),
-            ]),
-    .testTarget(name: "winmd-inspectTests",
-            dependencies: [
-              "winmd-inspect",
-              "SQL",
-              "WinMD",
-            ],
-            swiftSettings: [
-              .enableExperimentalFeature("Lifetimes"),
-            ]),
-  ]
-)
+              // SQL
+              .target(name: "SQL", dependencies: [],
+                      swiftSettings: [
+                        .enableExperimentalFeature("Lifetimes"),
+                      ]),
+              .testTarget(name: "SQLTests", dependencies: ["SQL"],
+                          swiftSettings: [
+                            .enableExperimentalFeature("Lifetimes"),
+                          ]),
+
+              // WinMD
+              .target(name: "WinMD",
+                      dependencies: [
+                        "CPE",
+                      ],
+                      swiftSettings: [
+                        .enableExperimentalFeature("Lifetimes"),
+                      ]),
+              .testTarget(name: "WinMDTests", dependencies: ["WinMD"],
+                          swiftSettings: [
+                            .enableExperimentalFeature("Lifetimes"),
+                          ]),
+
+              // winmd-inspect
+              .executableTarget(name: "winmd-inspect",
+                                dependencies: [
+                                  "SQL",
+                                  "WinMD",
+                                  .product(name: "ArgumentParser",
+                                           package: "swift-argument-parser"),
+                                ],
+                                swiftSettings: [
+                                  .enableExperimentalFeature("Lifetimes"),
+                                ]),
+              .testTarget(name: "winmd-inspectTests",
+                          dependencies: [
+                            "winmd-inspect",
+                            "SQL",
+                            "WinMD",
+                          ],
+                          swiftSettings: [
+                            .enableExperimentalFeature("Lifetimes"),
+                          ]),
+            ])
